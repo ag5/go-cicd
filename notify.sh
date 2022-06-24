57005dc19640c3fcffa0d1e3a129388c383b79bd
lars <lars@ag5.com>
2022-06-24 14:35:59 +0200

    added module test

--- notify.sh
+++ notify.sh
@@ -10,5 +10,5 @@ EMAIL=$(git log -1 --format=%ae)
 aws lambda invoke \
     --function-name ag5-slack-notifier \
     --cli-binary-format raw-in-base64-out \
-    --payload  '{"email": "'${EMAIL}'","message": "{\"attachments\":[{\"text\":\"Oh no! The '${IMAGE_REPO_NAME}' build has failed!\",\"color\":\"danger\",\"mrkdwn_in\":[\"text\",\"fields\"],\"fields\":[{\"title\":\"Codebuild log\",\"value\":\"<'${CODEBUILD_BUILD_URL}'|View full log>\",\"short\":true}]}],\"text\":null,\"username\":\"go-codebuild\",\"icon_emoji\":\":aws_codebuild:\"}"}' \
+    --payload  '{"email": "'${EMAIL}'","message": "{\"attachments\":[{\"text\":\"Oh no! The '${IMAGE_REPO_NAME}' build has failed!\",\"color\":\"danger\",\"mrkdwn_in\":[\"text\",\"fields\"],\"fields\":[{\"title\":\"Codebuild log\",\"value\":\"<'${CODEBUILD_BUILD_URL}'|View full log>\",\"short\":true}]}],\"text\":null,\"username\":\"go-release-builder\",\"icon_emoji\":\":aws_codebuild:\"}"}' \
     response.json

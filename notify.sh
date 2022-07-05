c2439420de99b94297aa3549d2744c9fc681964a
lars <lars@ag5.com>
2022-07-05 07:33:55 +0200

    Message on success

--- notify.sh
+++ notify.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ "$CODEBUILD_BUILD_SUCCEEDING" == 1 ]
-    then
-        exit 0
-fi
-
 EMAIL=$(git log -1 --format=%ae)
 
-aws lambda invoke \
-    --function-name ag5-slack-notifier \
-    --cli-binary-format raw-in-base64-out \
-    --payload  '{"email": "'${EMAIL}'","message": "{\"attachments\":[{\"text\":\"Oh no! The '${IMAGE_REPO_NAME}' build has failed!\",\"color\":\"danger\",\"mrkdwn_in\":[\"text\",\"fields\"],\"fields\":[{\"title\":\"Codebuild log\",\"value\":\"<'${CODEBUILD_BUILD_URL}'|View full log>\",\"short\":true}]}],\"text\":null,\"username\":\"go-release-builder\",\"icon_emoji\":\":aws_codebuild:\"}"}' \
-    response.json
+if [ "$CODEBUILD_BUILD_SUCCEEDING" == 1 ]
+    then
+        aws lambda invoke \
+            --function-name ag5-slack-notifier \
+            --cli-binary-format raw-in-base64-out \
+            --payload  '{"email": "'${EMAIL}'","message": "{\"attachments\":[{\"text\":\"The '${IMAGE_REPO_NAME}' has succeeded!\",\"color\":\"good\",\"mrkdwn_in\":[\"text\",\"fields\"],\"fields\":[{\"title\":\"Codebuild log\",\"value\":\"<'${CODEBUILD_BUILD_URL}'|View full log>\",\"short\":true}]}],\"text\":null,\"username\":\"go-release-builder\",\"icon_emoji\":\":aws_codebuild:\"}"}' \
+            response.json
+else
+    aws lambda invoke \
+        --function-name ag5-slack-notifier \
+        --cli-binary-format raw-in-base64-out \
+        --payload  '{"email": "'${EMAIL}'","message": "{\"attachments\":[{\"text\":\"Oh no! The '${IMAGE_REPO_NAME}' build has failed!\",\"color\":\"danger\",\"mrkdwn_in\":[\"text\",\"fields\"],\"fields\":[{\"title\":\"Codebuild log\",\"value\":\"<'${CODEBUILD_BUILD_URL}'|View full log>\",\"short\":true}]}],\"text\":null,\"username\":\"go-release-builder\",\"icon_emoji\":\":aws_codebuild:\"}"}' \
+        response.json
+fi
